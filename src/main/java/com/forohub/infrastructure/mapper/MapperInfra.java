fa04cdbe999c287ebab51bb71e3c6e476bbb2d2d
Diego Diomir <aries-d2github@hotmail.com>
2025-07-07 15:06:21 -0400

    metodo fromCursoToCursoRes agregado

---
+++ src/main/java/com/forohub/infrastructure/mapper/MapperInfra.java
@@ -0,0 +1,16 @@
+package com.forohub.infrastructure.mapper;
+
+import com.forohub.domain.model.Curso;
+import com.forohub.infrastructure.model.CursoRes;
+
+public class MapperInfra {
+
+    public static CursoRes fromCursoToCursoRes(Curso curso) {
+        return CursoRes.builder()
+                .id(curso.getId())
+                .nombre(curso.getNombre())
+                .categoria(curso.getCategoria())
+                .build();
+    }
+
+}
